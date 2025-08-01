f47064bdc99f516570be3b65f6a82011d25f9984
Suraj7149 <santoshsuraj7149@gmail.com>
2025-08-01 16:50:28 +0530

    Kind off Fixed the art component

--- src/Components/Art.jsx
+++ src/Components/Art.jsx
@@ -23,18 +23,18 @@ const Art = () => {
 	maskTimeline
 	 .to('.will-fade', { opacity: 0, stagger: 0.2, ease: 'power1.inOut', })
 	 .to('.masked-img', { scale: 1.3, maskPosition: 'center', maskSize: '400%', duration: 1, ease: 'power1.inOut '})
-	 .to('#masked-content', { opacity: 1, duration: 1, ease: 'power1.inOut'})
+	 .to('#masked-content', { opacity: 1, duration: 1, ease: 'power1.inOut', paddingBottom: "20px"})
  })
  
  return (
 	<div id="art">
-	 <div className="container mx-auto h-full pt-20">
+	 <div className="container mx-auto h-full md:pt-20">
 		<h2 className="will-fade">The ART</h2>
 		
 		<div className="content">
-		 <ul className="space-y-4 will-fade">
+		 <ul className="md:space-y-4 will-fade">
 			{goodLists.map((feature, index) => (
-			 <li key={index} className="flex items-center gap-2">
+			 <li key={index} className="flex items-center md:gap-2">
 				<img src="/images/check.png" alt="check" />
 				<p>{feature}</p>
 			 </li>
@@ -49,7 +49,7 @@ const Art = () => {
 			/>
 		 </div>
 		 
-		 <ul className="space-y-4 will-fade">
+		 <ul className="md:space-y-4 will-fade">
 			{featureLists.map((feature, index) => (
 			 <li key={index} className="flex items-center justify-start gap-2">
 				<img src="/images/check.png" alt="check" />
